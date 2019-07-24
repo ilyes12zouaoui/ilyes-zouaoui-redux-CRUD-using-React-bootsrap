79dd8313a93142e7ad3ef633911043ecffc79315
ilyes12zouaoui <ilyes.zouaoui@esprit.tn>
2019-07-24 15:44:35 +0200

    Updates

---
+++ precache-manifest.a1ecf4e2d1b1fbd9cf2448e13f8f2c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017f0fa194775d8d23c8aad658bbd51a",
+    "url": "/ilyes-zouaoui-redux-CRUD-using-React-bootsrap/index.html"
+  },
+  {
+    "revision": "c0e2130b1ef4f22f0d21",
+    "url": "/ilyes-zouaoui-redux-CRUD-using-React-bootsrap/static/css/main.f9f2407b.chunk.css"
+  },
+  {
+    "revision": "dc2f2791858d7c5096b0",
+    "url": "/ilyes-zouaoui-redux-CRUD-using-React-bootsrap/static/js/2.47306421.chunk.js"
+  },
+  {
+    "revision": "c0e2130b1ef4f22f0d21",
+    "url": "/ilyes-zouaoui-redux-CRUD-using-React-bootsrap/static/js/main.70659992.chunk.js"
+  },
+  {
+    "revision": "f8a5fbb56628a1ef9703",
+    "url": "/ilyes-zouaoui-redux-CRUD-using-React-bootsrap/static/js/runtime~main.2d3ec8c7.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ilyes-zouaoui-redux-CRUD-using-React-bootsrap/static/media/logo.ee7cd8ed.svg"
+  }
+]);
